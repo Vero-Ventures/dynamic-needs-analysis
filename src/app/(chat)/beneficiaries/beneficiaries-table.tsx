ddc1247377e0116a3cd0d60f2e13bf3b440b53af
scottchen98 <scott.chen1117@gmail.com>
2024-06-06 16:42:00 -0700

    implement dynamic allocation of beneficiaries in BeneficiariesTable component

--- src/app/(chat)/beneficiaries/beneficiaries-table.tsx
+++ src/app/(chat)/beneficiaries/beneficiaries-table.tsx
@@ -12,31 +12,91 @@ import {
   TableFooter,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
+import { useState } from "react";
+
+export type Beneficiary = {
+  name: string;
+  allocation: number;
+};
 export default function BeneficiariesTable() {
+  const [beneficiaries, setBeneficiaries] = useState<Beneficiary[]>([]);
+  const [totalAllocationPercentage, setTotalAllocationPercentage] = useState(0);
+  const [name, setName] = useState("");
+  const [allocation, setAllocation] = useState(0);
+
+  const newTotalAllocationPercentage = totalAllocationPercentage + allocation;
+  const remainingAllocationPercentage = (
+    100 - totalAllocationPercentage
+  ).toFixed(2);
+
   return (
     <Table className="mx-auto max-w-2xl">
       <TableHeader>
         <TableRow>
           <TableHead className="text-center">Name</TableHead>
           <TableHead className="text-center">Allocation (%)</TableHead>
-          <TableHead className="text-right">Action</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        <BeneficiaryTableRow name="Jane Doe" allocation={50} />
-        <BeneficiaryTableRow name="John Don" allocation={30} />
-        <BeneficiaryTableRow name="Alice Kan" allocation={20} />
+        {beneficiaries.map((b) => (
+          <BeneficiaryTableRow
+            key={b.name}
+            name={b.name}
+            allocation={b.allocation}
+          />
+        ))}
+        <TableRow>
+          <TableCell className="text-center font-medium">
+            <Input
+              type="text"
+              className="w-full border border-x-0 border-t-0 p-1"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Add New Beneficiary"
+            />
+          </TableCell>
+          <TableCell className="text-center font-medium">
+            <Input
+              type="number"
+              min="0"
+              max={remainingAllocationPercentage}
+              className="w-full border border-x-0 border-t-0 p-1"
+              onChange={(e) => setAllocation(+e.target.value)}
+              value={allocation || ""}
+              placeholder={`${remainingAllocationPercentage}%`}
+            />
+          </TableCell>
+          <TableCell></TableCell>
+        </TableRow>
       </TableBody>
       <TableFooter>
         <TableRow>
-          <TableCell colSpan={2}>
+          <TableCell>
             <p className="text-base font-bold">
-              Total: <span>100%</span>
+              Total: <span>{totalAllocationPercentage}%</span>
             </p>
           </TableCell>
+          <TableCell></TableCell>
           <TableCell className="text-right">
-            <Button type="submit" className="">
+            <Button
+              type="submit"
+              disabled={newTotalAllocationPercentage > 100}
+              onClick={() => {
+                if (newTotalAllocationPercentage > 100) return;
+
+                setTotalAllocationPercentage(
+                  +newTotalAllocationPercentage.toFixed(2),
+                );
+                setBeneficiaries((beneficiaries) => [
+                  ...beneficiaries,
+                  { name, allocation },
+                ]);
+                setName("");
+                setAllocation(0);
+              }}
+            >
               Add New Beneficiaries
             </Button>
           </TableCell>
@@ -46,11 +106,7 @@ export default function BeneficiariesTable() {
   );
 }
 
-type BeneficiaryTableRowProps = {
-  name: string;
-  allocation: number;
-};
-function BeneficiaryTableRow({ name, allocation }: BeneficiaryTableRowProps) {
+function BeneficiaryTableRow({ name, allocation }: Beneficiary) {
   return (
     <TableRow>
       <TableCell className="text-center font-medium">{name}</TableCell>
