74f0af89cf8177be1be5bcca578eebdf5e1580e6
scottchen98 <scott.chen1117@gmail.com>
2024-06-06 14:41:15 -0700

    implement beneficiaries table

--- src/app/(chat)/beneficiaries/beneficiaries-table.tsx
+++ src/app/(chat)/beneficiaries/beneficiaries-table.tsx
@@ -1,3 +1,53 @@
+"use client";
+
+import { CircleXIcon } from "lucide-react";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableFooter,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+
 export default function BeneficiariesTable() {
-  return <div>BeneficiariesTable</div>;
+  return (
+    <Table className="mx-auto max-w-2xl">
+      <TableHeader>
+        <TableRow>
+          <TableHead className="text-center">Name</TableHead>
+          <TableHead className="text-center">Allocation (%)</TableHead>
+          <TableHead className="text-right">Action</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell className="text-center font-medium">Jane Doe</TableCell>
+          <TableCell className="text-center font-medium">100</TableCell>
+          <TableCell className="text-right">
+            <Button variant="link" className="p-0">
+              <CircleXIcon className="text-destructive" />
+            </Button>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={2}>
+            <p className="text-base font-bold">
+              Total: <span>100%</span>
+            </p>
+          </TableCell>
+          <TableCell className="text-right">
+            <Button type="submit" className="">
+              Add New Beneficiaries
+            </Button>
+          </TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  );
 }
