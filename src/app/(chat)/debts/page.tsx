907820b0e574335c73b903365db14234dd4e0248
scottchen98 <scott.chen1117@gmail.com>
2024-06-07 15:43:22 -0700

    add "Add New Debt" button to Debts page

--- src/app/(chat)/debts/page.tsx
+++ src/app/(chat)/debts/page.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export default function Debts() {
-  return <div>Debts</div>;
+  return (
+    <div className="m-3">
+      <Link href="/debts/add">
+        <Button>Add New Debt</Button>
+      </Link>
+    </div>
+  );
 }
