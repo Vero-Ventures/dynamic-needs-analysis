eb57b67a5e4231698755ec43920f28e6ff6cc7de
scottchen98 <scott.chen1117@gmail.com>
2024-06-07 11:15:26 -0700

    refactor Beneficiaries component to use BeneficiariesPage

---
+++ src/app/(chat)/beneficiaries/beneficiaries.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useState } from "react";
+import type { Beneficiary } from "./beneficiaries-table";
+import BeneficiariesTable from "./beneficiaries-table";
+import DesiredBeneficiaryAllocationChart from "./desired-beneficiary-allocation-chart";
+
+export default function Beneficiaries() {
+  const [beneficiaries, setBeneficiaries] = useState<Beneficiary[]>([]);
+  return (
+    <div>
+      <BeneficiariesTable
+        beneficiaries={beneficiaries}
+        setBeneficiaries={setBeneficiaries}
+      />
+      <DesiredBeneficiaryAllocationChart beneficiaries={beneficiaries} />
+    </div>
+  );
+}
--- src/app/(chat)/beneficiaries/page.tsx
+++ src/app/(chat)/beneficiaries/page.tsx
@@ -1,19 +1,5 @@
-"use client";
-
-import { useState } from "react";
-import type { Beneficiary } from "./beneficiaries-table";
-import BeneficiariesTable from "./beneficiaries-table";
-import DesiredBeneficiaryAllocationChart from "./desired-beneficiary-allocation-chart";
+import Beneficiaries from "./beneficiaries";
 
 export default function BeneficiariesPage() {
-  const [beneficiaries, setBeneficiaries] = useState<Beneficiary[]>([]);
-  return (
-    <div>
-      <BeneficiariesTable
-        beneficiaries={beneficiaries}
-        setBeneficiaries={setBeneficiaries}
-      />
-      <DesiredBeneficiaryAllocationChart beneficiaries={beneficiaries} />
-    </div>
-  );
+  return <Beneficiaries />;
 }
