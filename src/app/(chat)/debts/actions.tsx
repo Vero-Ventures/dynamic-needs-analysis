b8b7d436e0c89a71a5e7dd913bafcacfe7ea84fd
scottchen98 <scott.chen1117@gmail.com>
2024-06-07 19:34:27 -0700

    add actions for adding and deleting debts

---
+++ src/app/(chat)/debts/actions.tsx
@@ -0,0 +1,47 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { AddDebtSchema, debtsData } from "../../data/db";
+
+export async function addDebt(data: FormData) {
+  const formData = Object.fromEntries(data.entries());
+  const parsed = AddDebtSchema.safeParse(formData);
+  if (!parsed.success) {
+    const fields: Record<string, string> = {};
+    for (const key of Object.keys(formData)) {
+      fields[key] = formData[key].toString();
+    }
+    return { message: "Invalid form data", fields };
+  }
+
+  const {
+    name,
+    initialValue,
+    yearAcquired,
+    rate,
+    term,
+    annualPayment,
+    insurableFutureValueDollars,
+  } = parsed.data;
+
+  debtsData.push({
+    id: debtsData.length,
+    name,
+    initialValue,
+    yearAcquired,
+    rate,
+    term,
+    annualPayment,
+    insurableFutureValueDollars,
+  });
+  revalidatePath("/debts");
+}
+
+export async function deleteDebt(id: number) {
+  const i = debtsData.findIndex((g) => g.id === id);
+  if (i === -1) {
+    throw new Error("No goal found at this index");
+  }
+  debtsData.splice(i, 1);
+  revalidatePath("/debts");
+}
