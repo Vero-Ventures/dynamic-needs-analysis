1add51a0717923cc510485f3e896ab52d2766230
scottchen98 <scott.chen1117@gmail.com>
2024-06-07 18:23:58 -0700

    refactor Debts page component name to DebtsPage

--- src/app/(chat)/debts/page.tsx
+++ src/app/(chat)/debts/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
-export default function Debts() {
+export default function DebtsPage() {
   return (
     <div className="m-3">
       <Link href="/debts/add">
