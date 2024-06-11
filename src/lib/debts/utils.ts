f919621578a49ac65c7087b0507d31b8b5c07213
scottchen98 <scott.chen1117@gmail.com>
2024-06-11 15:23:23 -0700

    rename debt calculation utility functions

--- src/lib/debts/utils.ts
+++ src/lib/debts/utils.ts
@@ -1,48 +1,48 @@
-export function currentYearsHeld(yearAcquired: number): number {
+export function calculateCurrentYearsHeld(yearAcquired: number): number {
   const currentYear: number = new Date().getFullYear();
   return currentYear - yearAcquired;
 }
 
-export function amountPaidOffDollars(
+export function calculateAmountPaidOffDollars(
   annualPayment: number,
-  currentYearsHeld: number,
+  currentYearsHeld: number
 ): number {
   return annualPayment * currentYearsHeld;
 }
 
-export function currentValueOfDebtDollars(
+export function calculateCurrentValueOfDebtDollars(
   initialValue: number,
   interestRate: number,
-  currentYearsHeld: number,
+  currentYearsHeld: number
 ): number {
   return initialValue * Math.pow(1 + interestRate / 100, currentYearsHeld);
 }
 
-export function debtRemainingDollars(
+export function calculateDebtRemainingDollars(
   currentValueOfDebtDollars: number,
-  amountPaidOffDollars: number,
+  amountPaidOffDollars: number
 ): number {
   return Math.max(0, currentValueOfDebtDollars - amountPaidOffDollars);
 }
 
-export function yearsToBePaidOff(
+export function calculateYearsToBePaidOff(
   interestRate: number,
   annualPayment: number,
-  debtRemainingDollars: number,
+  debtRemainingDollars: number
 ): number {
   if (annualPayment <= 0 || debtRemainingDollars <= 0) return 0;
 
-  return numberOfPaymentPeriods(
+  return calculateNumberOfPaymentPeriods(
     interestRate,
     annualPayment,
-    debtRemainingDollars,
+    debtRemainingDollars
   );
 }
 
-export function numberOfPaymentPeriods(
+export function calculateNumberOfPaymentPeriods(
   interestRate: number,
   annualPayment: number,
-  presentValue: number,
+  presentValue: number
 ): number {
   interestRate = interestRate / 100;
 
@@ -51,23 +51,23 @@ export function numberOfPaymentPeriods(
   }
 
   const numerator: number = Math.log(
-    annualPayment / (annualPayment - presentValue * interestRate),
+    annualPayment / (annualPayment - presentValue * interestRate)
   );
   const denominator: number = Math.log(1 + interestRate);
   return numerator / denominator;
 }
 
-export function futureValueOfActualTermDebtDollars(
+export function calculateFutureValueOfActualTermDebtDollars(
   initialValue: number,
   interestRate: number,
-  term: number,
+  term: number
 ): number {
   return initialValue * Math.pow(1 + interestRate / 100, term);
 }
 
-export function insurableFutureValueDollars(
+export function calculateInsurableFutureValueDollars(
   futureValueOfActualTermDebtDollars: number,
-  amountPaidOffDollars: number,
+  amountPaidOffDollars: number
 ): number {
   return futureValueOfActualTermDebtDollars - amountPaidOffDollars;
 }
