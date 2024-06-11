7abeb6ca9a70ee6100dc0e7ab260bac361cc3e28
Michael Lei <michaelleie0ca@gmail.com>
2024-06-10 23:07:08 -0700

    Finish goals calculations

--- src/lib/goals/utils.ts
+++ src/lib/goals/utils.ts
@@ -1,7 +1,21 @@
-import type { Goals } from "@/app/data/db";
+import type { Asset, Goals } from "@/app/data/db";
 
 export function calculateTotalSumGoals(data: Goals[]) {
   return data.reduce((total, goal) => total + goal.amount, 0);
+}
+
+export function calculateLiquidityPreserved(
+  allocationFactor: number,
+  totalFutureValueLiquid: number,
+) {
+  return totalFutureValueLiquid * (1 - allocationFactor);
+}
+
+export function calculateLiquidityAllocatedToGoals(
+  allocationFactor: number,
+  totalFutureValueLiquid: number,
+) {
+  return totalFutureValueLiquid * allocationFactor;
 }
 
 export function calculateSurplusShortfall(
@@ -9,4 +23,35 @@ export function calculateSurplusShortfall(
   totalSumGoals: number,
 ) {
   return liquidityAllocated - totalSumGoals;
+}
+
+export function calculateTotalsFromAssets(assets: Asset[]) {
+  const result = {
+    totalCurrentValueLiquid: 0,
+    totalFutureValueLiquid: 0,
+    totalCurrentValueFixed: 0,
+    totalFutureValueFixed: 0,
+    totalCurrentValueToBeSold: 0,
+    totalFutureValueToBeSold: 0,
+  };
+  assets.forEach((asset: Asset): void => {
+    const futureValue: number =
+      asset.currentValue * Math.pow(1 + asset.rate / 100, asset.term);
+
+    if (asset.isLiquid) {
+      result.totalCurrentValueLiquid += asset.currentValue;
+      result.totalFutureValueLiquid += futureValue;
+    }
+
+    if (!asset.isLiquid && !asset.isToBeSold) {
+      result.totalCurrentValueFixed += asset.currentValue;
+      result.totalFutureValueFixed += futureValue;
+    }
+
+    if (asset.isToBeSold) {
+      result.totalCurrentValueToBeSold += asset.currentValue;
+      result.totalFutureValueToBeSold += futureValue;
+    }
+  });
+  return result;
 }
