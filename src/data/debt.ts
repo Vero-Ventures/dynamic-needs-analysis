39d5b2d1a9a0610d063b48af3207d2086ba75c7c
Michael Lei <michaelleie0ca@gmail.com>
2024-06-13 16:49:07 -0700

    remove db file

--- src/data/debt.ts
+++ src/data/debt.ts
@@ -1,5 +1,7 @@
+import { createClient } from "@/lib/supabase/server";
 import { debts } from "./db";
 
 export default async function getDebt(id: number) {
+  const sb = createClient();
   return debts.at(id);
 }
