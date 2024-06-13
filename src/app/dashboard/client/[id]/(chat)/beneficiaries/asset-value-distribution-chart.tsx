b4768fba5d06a25f964ca354a62a730a0c410cbf
Michael Lei <michaelleie0ca@gmail.com>
2024-06-12 21:11:32 -0700

    Finish charts for beneficiaries

--- src/app/dashboard/client/[id]/(chat)/beneficiaries/asset-value-distribution-chart.tsx
+++ src/app/dashboard/client/[id]/(chat)/beneficiaries/asset-value-distribution-chart.tsx
@@ -1,3 +1,41 @@
-export default function AssetValueDistributionChart() {
-  return <div>AssetValueDistributionChart</div>;
+"use client";
+
+import type { Asset } from "@/app/data/db";
+import { generateAssetValueDistributionSeries } from "@/lib/beneficiaries/utils";
+import ReactApexChart from "react-apexcharts";
+
+export default function AssetValueDistributionChart({
+  assets,
+}: {
+  assets: Asset[];
+}) {
+  const series = generateAssetValueDistributionSeries(assets);
+  return (
+    <ReactApexChart
+      options={{
+        chart: {
+          stacked: true,
+          animations: {
+            enabled: false,
+          },
+          toolbar: {
+            show: false,
+          },
+        },
+        plotOptions: { bar: { horizontal: true } },
+        dataLabels: { enabled: false },
+        xaxis: {
+          type: "category",
+          categories: assets.map((asset: Asset) => asset.name),
+        },
+        yaxis: { title: { text: "Percentage" }, max: 100 },
+        title: { text: "Beneficiary Allocation Percentage" },
+        legend: { position: "bottom" },
+        labels: [],
+      }}
+      series={series}
+      type="bar"
+      height={350}
+    />
+  );
 }
---
+++ src/lib/beneficiaries/utils.ts
@@ -0,0 +1,65 @@
+import type { AssetBeneficiary } from "@/app/dashboard/client/[id]/(chat)/assets/add/add-assets-stepper";
+import type { Asset, Beneficiary } from "@/app/data/db";
+
+export function generateDesiredDistributionSeriesAndLabels(
+  beneficiaries: Beneficiary[]
+) {
+  const beneficiaryTotals: Record<string, number> = {};
+
+  beneficiaries.forEach((beneficiary: Beneficiary): void => {
+    if (!beneficiaryTotals[beneficiary.name]) {
+      beneficiaryTotals[beneficiary.name] = 0;
+    }
+    beneficiaryTotals[beneficiary.name] += beneficiary.allocation;
+  });
+
+  const series = Object.values(beneficiaryTotals);
+  const labels = Object.keys(beneficiaryTotals);
+
+  return { series, labels };
+}
+
+export function generateRealDistributionSeriesAndLabels(assets: Asset[]) {
+  const beneficiaryTotals: Record<string, number> = {};
+
+  assets.forEach((asset: Asset): void => {
+    asset.assetBeneficiaries.forEach((beneficiary: AssetBeneficiary): void => {
+      if (!beneficiaryTotals[beneficiary.name]) {
+        beneficiaryTotals[beneficiary.name] = 0;
+      }
+      beneficiaryTotals[beneficiary.name] +=
+        (beneficiary.allocation / 100) * asset.currentValue;
+    });
+  });
+
+  const series = Object.values(beneficiaryTotals);
+  const labels = Object.keys(beneficiaryTotals);
+
+  return { series, labels };
+}
+
+export function generateAssetValueDistributionSeries(assets: Asset[]) {
+  const beneficiaryNames: string[] = [];
+  const series: { name: string; data: number[] }[] = [];
+
+  assets.forEach((asset: Asset): void => {
+    asset.assetBeneficiaries.forEach((beneficiary: Beneficiary): void => {
+      if (!beneficiaryNames.includes(beneficiary.name)) {
+        beneficiaryNames.push(beneficiary.name);
+        series.push({ name: beneficiary.name, data: [] });
+      }
+    });
+  });
+
+  assets.forEach((asset: Asset): void => {
+    series.forEach((series: { name: string; data: number[] }): void => {
+      const beneficiary: Beneficiary | undefined =
+        asset.assetBeneficiaries.find(
+          (b: Beneficiary): boolean => b.name === series.name
+        );
+      series.data.push(beneficiary ? beneficiary.allocation : 0);
+    });
+  });
+
+  return series;
+}
