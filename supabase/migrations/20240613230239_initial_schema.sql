05f80b505a49f75cd61f0216cc57067e3f6c69f4
Michael Lei <michaelleie0ca@gmail.com>
2024-06-13 16:03:24 -0700

    Generate initial and rls schema

--- supabase/migrations/20240613230239_initial_schema.sql
+++ supabase/migrations/20240613230239_initial_schema.sql
@@ -487,4 +487,68 @@ grant truncate on table "public"."shareholders" to "service_role";
 
 grant update on table "public"."shareholders" to "service_role";
 
+create policy "Enable all CRUD for all users"
+on "public"."asset_beneficiaries"
+as permissive
+for all
+to public
+using (true);
+
+
+create policy "Enable all CRUD for all users"
+on "public"."assets"
+as permissive
+for all
+to public
+using (true);
+
+
+create policy "Enable all CRUD for all users"
+on "public"."beneficiaries"
+as permissive
+for all
+to public
+using (true);
+
+
+create policy "Enable all CRUD for all users"
+on "public"."businesses"
+as permissive
+for all
+to public
+using (true);
+
+
+create policy "Enable all CRUD for all users"
+on "public"."clients"
+as permissive
+for all
+to public
+using (true);
+
+
+create policy "Enable all CRUD for all users"
+on "public"."debts"
+as permissive
+for all
+to public
+using (true);
+
+
+create policy "Enable all CRUD for all users"
+on "public"."goals"
+as permissive
+for all
+to public
+using (true);
+
+
+create policy "Enable all CRUD for all users"
+on "public"."shareholders"
+as permissive
+for all
+to public
+using (true);
+
+
 
