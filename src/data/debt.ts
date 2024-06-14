260bb3b1b672311ebd1876302405fee87a063efd
Michael Lei <michaelleie0ca@gmail.com>
2024-06-13 17:10:59 -0700

    Add types from generated from supabase

--- src/data/debt.ts
+++ src/data/debt.ts
@@ -1,7 +1,5 @@
 import { createClient } from "@/lib/supabase/server";
-import { debts } from "./db";
 
 export default async function getDebt(id: number) {
   const sb = createClient();
-  return debts.at(id);
 }
