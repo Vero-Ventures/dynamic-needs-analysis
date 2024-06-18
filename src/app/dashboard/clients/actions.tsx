129a6fd962141513b79ad09ff14b634380a92282
scottchen98 <scott.chen1117@gmail.com>
2024-06-17 22:33:59 -0700

    update addClient function to use Kinde authentication for kinde_id

--- src/app/dashboard/clients/actions.tsx
+++ src/app/dashboard/clients/actions.tsx
@@ -1,13 +1,19 @@
 "use server";
 
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
 import { createClient } from "@/lib/supabase/server";
 import type { CreateClient } from "@/types/db";
 import { revalidatePath } from "next/cache";
 
-export async function addClient(client: Omit<CreateClient, "agent_id">) {
+export async function addClient(client: Omit<CreateClient, "kinde_id">) {
   const sb = createClient();
-  // TODO: write query to get kinde_id of the current user
-  const kinde_id = 1;
+  const { getUser } = await getKindeServerSession();
+  const agent = await getUser();
+  if (!agent) {
+    throw new Error("User not found");
+  }
+  const kinde_id = agent.id;
   await sb.from("clients").insert({ kinde_id, ...client });
   revalidatePath("/dashboard/clients");
 }
