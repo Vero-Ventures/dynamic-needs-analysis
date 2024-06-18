6268b066e0833cc29c860cb583cdc9129af2d9c8
scottchen98 <scott.chen1117@gmail.com>
2024-06-17 19:18:52 -0700

    create BeneficiariesTable component

---
+++ src/app/dashboard/client/[id]/(client)/edit/beneficiaries-table.tsx
@@ -0,0 +1,82 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { cn } from "@/lib/utils";
+import DeleteBeneficiaryButton from "@/components/delete-item-button";
+import EditBeneficiaryDialog from "./beneficiary-edit-dialog";
+import type { Tables } from "../../../../../../../types/supabase";
+
+export default function BeneficiariesTable({
+  beneficiaries,
+}: {
+  beneficiaries: Omit<Tables<"beneficiaries">, "created_at">[];
+}) {
+  return (
+    <Table className="mx-0">
+      <TableHeader>
+        <TableRow>
+          <TableHead
+            className={cn("text-left", {
+              "text-center": beneficiaries.length !== 0,
+            })}
+          >
+            Name
+          </TableHead>
+          <TableHead
+            className={cn("text-right", {
+              "text-center": beneficiaries.length !== 0,
+            })}
+          >
+            Allocation (parts)
+          </TableHead>
+          <TableHead></TableHead>
+          <TableHead></TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {beneficiaries.map((b) => (
+          <BeneficiaryTableRow
+            key={b.id}
+            id={b.id}
+            name={b.name}
+            allocation={b.allocation}
+          />
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
+
+function BeneficiaryTableRow({
+  id,
+  name,
+  allocation,
+}: Omit<Tables<"beneficiaries">, "created_at">) {
+  return (
+    <TableRow>
+      <TableCell className="text-center font-medium">{name}</TableCell>
+      <TableCell className="text-center font-medium">{allocation}</TableCell>
+      <TableCell className="text-center">
+        <EditBeneficiaryDialog id={id} name={name} allocation={allocation} />
+      </TableCell>
+      <TableCell className="text-right">
+        <DeleteBeneficiary id={id} />
+      </TableCell>
+    </TableRow>
+  );
+}
+
+function DeleteBeneficiary({ id }: { id: number }) {
+  // const deleteBeneficiaryWithBind = deleteBeneficiary.bind(null, id);
+  console.log(id);
+  return (
+    <form>
+      <DeleteBeneficiaryButton />
+    </form>
+  );
+}
