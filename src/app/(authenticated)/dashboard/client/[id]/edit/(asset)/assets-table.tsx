64ec9744cf6c7c71454e0c864c7e98ef0080b790
scottchen98 <scott.chen1117@gmail.com>
2024-06-19 15:11:30 -0700

    add delete functionality to assets table

--- src/app/(authenticated)/dashboard/client/[id]/edit/(asset)/assets-table.tsx
+++ src/app/(authenticated)/dashboard/client/[id]/edit/(asset)/assets-table.tsx
@@ -7,6 +7,8 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import DeleteAssetButton from "@/components/delete-item-button";
+import { formatMoney } from "@/lib/utils";
 
 export default function AssetsTable() {
   return (
@@ -21,7 +23,9 @@ export default function AssetsTable() {
           <TableHead className="text-right"></TableHead>
         </TableRow>
       </TableHeader>
-      <TableBody></TableBody>
+      <TableBody>
+        <AssetTableRow />
+      </TableBody>
       <TableFooter>
         <TableRow>
           <TableCell className="text-center">Total</TableCell>
@@ -30,5 +34,23 @@ export default function AssetsTable() {
         </TableRow>
       </TableFooter>
     </Table>
+  );
+}
+
+function AssetTableRow() {
+  return (
+    <TableRow>
+      <TableCell className="text-center font-medium">AAPL Stock</TableCell>
+      <TableCell className="text-center font-medium">2022</TableCell>
+      <TableCell className="text-center font-medium">
+        {formatMoney(100000)}
+      </TableCell>
+      <TableCell className="text-center font-medium">
+        {formatMoney(150000)}
+      </TableCell>
+      <TableCell className="text-right">
+        <DeleteAssetButton size="icon" />
+      </TableCell>
+    </TableRow>
   );
 }
