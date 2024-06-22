765edc57455951b9a1c4d9ce72878ac30c81fb1a
scottchen98 <scott.chen1117@gmail.com>
2024-06-21 18:22:25 -0700

    create deleteGoal action and server logic

--- src/app/(authenticated)/dashboard/client/[id]/new/(goals-and-philanthropy)/actions.ts
+++ src/app/(authenticated)/dashboard/client/[id]/new/(goals-and-philanthropy)/actions.ts
@@ -4,6 +4,7 @@ import { createClient } from "@/lib/supabase/server";
 import { ownsClientProcedure } from "@/procedures/auth/actions";
 import { revalidatePath } from "next/cache";
 import { createGoalSchema } from "./schema";
+import { z } from "zod";
 
 export const createGoal = ownsClientProcedure
   .createServerAction()
@@ -16,6 +17,23 @@ export const createGoal = ownsClientProcedure
       console.error(error.message);
       throw new Error(
         "Something went wrong with adding the goal to the database"
+      );
+    }
+
+    revalidatePath(`/dashboard/client/new/${input.client_id}`);
+  });
+
+export const deleteGoal = ownsClientProcedure
+  .createServerAction()
+  .input(z.object({ goal_id: z.number() }))
+  .handler(async ({ input }) => {
+    const sb = await createClient();
+    const { error } = await sb.from("goals").delete().eq("id", input.goal_id);
+
+    if (error) {
+      console.error(error.message);
+      throw new Error(
+        "Something went wrong with deleting the goal from the database"
       );
     }
 
