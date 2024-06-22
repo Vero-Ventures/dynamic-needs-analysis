5f8b7dde2ce91e00a661a26ddca316eb3b18ed46
scottchen98 <scott.chen1117@gmail.com>
2024-06-21 18:09:34 -0700

    create deleteBeneficiary action and server logic

--- src/app/(authenticated)/dashboard/client/[id]/new/(beneficiaries)/actions.ts
+++ src/app/(authenticated)/dashboard/client/[id]/new/(beneficiaries)/actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { z } from "zod";
 import { createClient } from "@/lib/supabase/server";
 import { ownsClientProcedure } from "@/procedures/auth/actions";
 import { revalidatePath } from "next/cache";
@@ -15,6 +16,24 @@ export const createBeneficiary = ownsClientProcedure
       console.error(error.message);
       throw new Error(
         "Something went wrong with adding the beneficiary to the database"
+      );
+    }
+    revalidatePath(`/dashboard/client/new/${input.client_id}`);
+  });
+
+export const deleteBeneficiary = ownsClientProcedure
+  .createServerAction()
+  .input(z.object({ beneficiary_id: z.number() }))
+  .handler(async ({ input }) => {
+    const sb = await createClient();
+    const { error } = await sb
+      .from("beneficiaries")
+      .delete()
+      .eq("id", input.beneficiary_id);
+    if (error) {
+      console.error(error.message);
+      throw new Error(
+        "Something went wrong with deleting the beneficiary from the database"
       );
     }
     revalidatePath(`/dashboard/client/new/${input.client_id}`);
