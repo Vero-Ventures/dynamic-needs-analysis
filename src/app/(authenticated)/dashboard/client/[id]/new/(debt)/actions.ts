e437c743757f111e688f544632134bf784f2df58
scottchen98 <scott.chen1117@gmail.com>
2024-06-21 18:15:01 -0700

    create deleteDebt action and server logic

--- src/app/(authenticated)/dashboard/client/[id]/new/(debt)/actions.ts
+++ src/app/(authenticated)/dashboard/client/[id]/new/(debt)/actions.ts
@@ -4,6 +4,7 @@ import { createClient } from "@/lib/supabase/server";
 import { ownsClientProcedure } from "@/procedures/auth/actions";
 import { revalidatePath } from "next/cache";
 import { createDebtSchema } from "./schema";
+import { z } from "zod";
 
 export const createDebt = ownsClientProcedure
   .createServerAction()
@@ -23,5 +24,20 @@ export const createDebt = ownsClientProcedure
       );
     }
 
+    revalidatePath(`/dashboard/client/new/${input.client_id}`);
+  });
+
+export const deleteDebt = ownsClientProcedure
+  .createServerAction()
+  .input(z.object({ debt_id: z.number() }))
+  .handler(async ({ input }) => {
+    const sb = await createClient();
+    const { error } = await sb.from("debts").delete().eq("id", input.debt_id);
+    if (error) {
+      console.error(error.message);
+      throw new Error(
+        "Something went wrong with deleting the debt from the database"
+      );
+    }
     revalidatePath(`/dashboard/client/new/${input.client_id}`);
   });
