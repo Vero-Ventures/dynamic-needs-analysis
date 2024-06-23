6c8d089b7c73ef999c26c93c774cbf3d6caeeda0
scottchen98 <scott.chen1117@gmail.com>
2024-06-23 11:02:14 -0700

    refactor asset editing logic and update beneficiaries in editAsset action

--- src/app/(authenticated)/dashboard/client/[id]/new/(asset)/actions.ts
+++ src/app/(authenticated)/dashboard/client/[id]/new/(asset)/actions.ts
@@ -33,6 +33,38 @@ export const createAsset = ownsClientProcedure
     revalidatePath(`/dashboard/client/new/${input.client_id}`);
   });
 
+export const editAsset = ownsClientProcedure
+  .createServerAction()
+  .input(
+    createAssetSchema
+      .extend({ asset_id: z.number() })
+      .merge(createAssetBeneficiarySchema)
+  )
+  .handler(async ({ input }) => {
+    const { asset_id, asset_beneficiaries, ...asset } = input;
+    // Update the asset table
+    const sb = await createClient();
+    const { error } = await sb.from("assets").update(asset).eq("id", asset_id);
+    if (error) {
+      console.error(error.message);
+      throw new Error(
+        "Something went wrong with updating the asset in the database"
+      );
+    }
+    // Remove all beneficiaries for the asset before re-adding them
+    await sb.from("asset_beneficiaries").delete().eq("asset_id", asset_id);
+    // Update asset beneficiaries with the asset id
+    const assetBeneficiariesWithAssetId = asset_beneficiaries.map((b) => {
+      return {
+        ...b,
+        asset_id: input.asset_id,
+      };
+    });
+    // Insert the updated asset beneficiaries
+    await sb.from("asset_beneficiaries").upsert(assetBeneficiariesWithAssetId);
+    revalidatePath(`/dashboard/client/new/${input.client_id}`);
+  });
+
 import { z } from "zod";
 export const deleteAsset = ownsClientProcedure
   .createServerAction()
