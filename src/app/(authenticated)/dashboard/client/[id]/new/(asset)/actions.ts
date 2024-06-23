afae312e0f9e6470a34cbf177198633200af5633
scottchen98 <scott.chen1117@gmail.com>
2024-06-22 17:20:07 -0700

    refactor createAsset action to include asset beneficiaries array

--- src/app/(authenticated)/dashboard/client/[id]/new/(asset)/actions.ts
+++ src/app/(authenticated)/dashboard/client/[id]/new/(asset)/actions.ts
@@ -1,39 +1,34 @@
+"use server";
+
 import { createClient } from "@/lib/supabase/server";
-import {
-  ownsAssetProcedure,
-  ownsClientProcedure,
-} from "@/procedures/auth/actions";
+import { ownsClientProcedure } from "@/procedures/auth/actions";
 import { revalidatePath } from "next/cache";
 import { createAssetBeneficiarySchema, createAssetSchema } from "./schema";
 
 export const createAsset = ownsClientProcedure
   .createServerAction()
-  .input(createAssetSchema)
+  .input(createAssetSchema.merge(createAssetBeneficiarySchema))
   .handler(async ({ input }) => {
+    const { asset_beneficiaries, ...asset } = input;
     const sb = await createClient();
-    const { error } = await sb.from("assets").insert(input);
-    if (error) {
+    const { data: addedAsset, error } = await sb
+      .from("assets")
+      .insert(asset)
+      .select("id")
+      .single();
+    if (!addedAsset || error) {
       console.error(error.message);
       throw new Error(
         "Something went wrong with adding the asset to the database"
       );
     }
-    revalidatePath(`/dashboard/client/new/${input.client_id}`);
-  });
 
-export const createAssetBeneficiary = ownsAssetProcedure
-  .createServerAction()
-  .input(createAssetBeneficiarySchema)
-  .handler(async ({ input }) => {
-    const sb = await createClient();
-    const { error } = await sb.from("asset_beneficiaries").insert(input);
-
-    if (error) {
-      console.error(error.message);
-      throw new Error(
-        "Something went wrong with adding the asset beneficiary to the database"
-      );
-    }
-
+    const assetBeneficiariesWithAssetId = asset_beneficiaries.map((b) => {
+      return {
+        ...b,
+        asset_id: addedAsset.id,
+      };
+    });
+    await sb.from("asset_beneficiaries").insert(assetBeneficiariesWithAssetId);
     revalidatePath(`/dashboard/client/new/${input.client_id}`);
   });
