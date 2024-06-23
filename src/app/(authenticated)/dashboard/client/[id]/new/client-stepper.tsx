c4f45e6b8699c7fc1af8f99e08e22311371ade75
scottchen98 <scott.chen1117@gmail.com>
2024-06-22 17:22:40 -0700

    add assets component to client stepper

--- src/app/(authenticated)/dashboard/client/[id]/new/client-stepper.tsx
+++ src/app/(authenticated)/dashboard/client/[id]/new/client-stepper.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import GoalsAndPhilanthropy from "./(goals-and-philanthropy)/goals-and-philanthropy";
 import { Loader2Icon } from "lucide-react";
 import Businesses from "./(businesses)/businesses";
+import Assets from "./(asset)/assets";
 import Debts from "./(debt)/debts";
 import StepperContainer from "./steppper-container";
 
@@ -31,7 +32,7 @@ export default function ClientStepper({
             "mx-auto max-h-[calc(100dvh-72px-100px-100px)] w-full max-w-3xl overflow-y-auto"
           )}
         >
-          {/* <Assets /> */}
+          <Assets clientId={clientId} />
         </div>
         <div
           className={cn(
