0cf65b9ab66dc07cf9869d327e4141485cdbff54
Michael Lei <michaelleie0ca@gmail.com>
2024-06-25 12:25:38 -0700

    Finish income replacement page

--- src/app/(authenticated)/dashboard/client/[id]/income-replacement/page.tsx
+++ src/app/(authenticated)/dashboard/client/[id]/income-replacement/page.tsx
@@ -1,40 +1,81 @@
-import { GiftIcon, InboxIcon, LandmarkIcon, PenBoxIcon } from "lucide-react";
+import {
+  calculateAgeFromDate,
+  calculateInsuredIncomeAmount,
+  calculateYearsOfActiveIncome,
+} from "@/lib/client/utils";
+import { createClient } from "@/lib/supabase/server";
+import { formatMoney } from "@/lib/utils";
+import { CircleDollarSignIcon, HandCoinsIcon, User2Icon } from "lucide-react";
+import { notFound } from "next/navigation";
 
-export default function IncomeReplacementPage() {
+export default async function IncomeReplacementPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const clientId = parseInt(params.id);
+  const sb = await createClient();
+  const { data: client, error } = await sb
+    .from("clients")
+    .select()
+    .eq("id", clientId)
+    .single();
+
+  if (error) {
+    throw error;
+  }
+
+  if (!client) {
+    return notFound();
+  }
+
+  const age = calculateAgeFromDate(new Date(client.birth_date));
+  const yearsOfActiveIncome = calculateYearsOfActiveIncome(
+    age,
+    client.expected_retirement_age
+  );
+  const amountInsuredForIncome = calculateInsuredIncomeAmount(
+    client.annual_income,
+    client.income_multiplier
+  );
+
   return (
     <div className="p-4">
       <h1 className="text-3xl font-bold">Income Replacement</h1>
-      <div className="mt-10 grid grid-cols-2 gap-20">
+      <div className="mt-10">
         <div>
           <h2 className="mb-4 border-b-2 border-primary pb-4 text-xl font-bold text-primary">
-            Analysis
+            Client Information
           </h2>
-          <div className="space-y-4">
+          <div className="grid gap-10 md:grid-cols-2 xl:grid-cols-3">
             <StatCard
-              value="$300k"
-              description="annual salary"
-              icon={<LandmarkIcon className="h-20 w-20 opacity-15" />}
+              icon={<User2Icon className="h-20 w-20 opacity-15" />}
+              value={`${client.name}, ${age}`}
+              description={client.province}
             />
             <StatCard
-              value="$0 - $700k"
-              description="discretionary bonus"
-              icon={<GiftIcon className="h-20 w-20 opacity-15" />}
+              value={client.expected_retirement_age.toString()}
+              description="Expected Retirement Age"
             />
             <StatCard
-              value="$300k - $3MM"
-              description="discretionary dividend"
-              icon={<PenBoxIcon className="h-20 w-20 opacity-15" />}
+              value={yearsOfActiveIncome.toString()}
+              description="Years of Active Income"
             />
             <StatCard
-              value="$2.5MM"
-              description="an average total income over the last 3 years, factoring in inflation and increases in earnings"
-              icon={<InboxIcon className="h-20 w-20 opacity-15" />}
+              icon={<CircleDollarSignIcon className="h-20 w-20 opacity-15" />}
+              value={formatMoney(client.annual_income)}
+              description="Annual Income"
+            />
+            <StatCard
+              value={client.income_multiplier.toString()}
+              description="Income Replacement Mutiplier"
+            />
+            <StatCard
+              icon={<HandCoinsIcon className="h-20 w-20 opacity-15" />}
+              value={formatMoney(amountInsuredForIncome)}
+              description="Amount insured for income"
             />
           </div>
-        </div>
-        <div className="h-fit w-fit space-y-4 rounded-3xl bg-secondary p-10 text-secondary-foreground">
-          <p className="text-6xl font-bold">$30MM</p>
-          <p className="text-sm">10-year term policy is most appropriate</p>
         </div>
       </div>
     </div>
@@ -48,17 +89,17 @@ function StatCard({
 }: {
   value: string;
   description: string;
-  icon: React.ReactNode;
+  icon?: React.ReactNode;
 }) {
   return (
-    <div>
-      <div className="flex items-center justify-between rounded-2xl bg-muted p-2">
-        <div className="p-4">
-          <div className="text-3xl font-bold">{value}</div>
-          <div className="text-sm">{description}</div>
+    <div className="flex h-full items-center justify-between rounded-2xl bg-muted p-2">
+      <div className="p-4">
+        <div className="text-3xl font-bold text-secondary dark:text-secondary-foreground">
+          {value}
         </div>
-        <div>{icon}</div>
+        <div className="text-sm">{description}</div>
       </div>
+      {icon && <div>{icon}</div>}
     </div>
   );
 }
