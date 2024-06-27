d09ef0334257ba037ee10de871d6f92f0a7aa75c
scottchen98 <scott.chen1117@gmail.com>
2024-06-27 15:58:15 -0700

    update validation for growth rate and time horizon in createBusinessSchema

--- src/app/(authenticated)/dashboard/client/[id]/edit/(businesses)/schema.ts
+++ src/app/(authenticated)/dashboard/client/[id]/edit/(businesses)/schema.ts
@@ -4,8 +4,14 @@ export const createBusinessSchema = z.object({
   name: z.string().trim().min(3, "Your name must be greater than 3 characters"),
   valuation: z.coerce.number(),
   ebitda: z.coerce.number(),
-  term: z.coerce.number(),
-  appreciation_rate: z.coerce.number(),
+  term: z.coerce
+    .number()
+    .min(0, { message: "Time Horizon must be greater than 0" })
+    .max(20, { message: "Time Horizon cannot exceed 20" }),
+  appreciation_rate: z.coerce
+    .number()
+    .min(0, { message: "Growth Rate must be between 0 and 6" })
+    .max(6, { message: "Growth Rate must be between 0 and 6" }),
 });
 
 export type CreateBusiness = z.infer<typeof createBusinessSchema>;
