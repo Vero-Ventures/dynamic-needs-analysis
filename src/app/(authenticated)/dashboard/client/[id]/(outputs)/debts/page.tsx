5ff58dea923e4702ae9435b946c81cbba6d436a6
scottchen98 <scott.chen1117@gmail.com>
2024-06-27 14:41:46 -0700

    refactor debts page to use Heading component

--- src/app/(authenticated)/dashboard/client/[id]/(outputs)/debts/page.tsx
+++ src/app/(authenticated)/dashboard/client/[id]/(outputs)/debts/page.tsx
@@ -1,6 +1,7 @@
 import DebtsTable from "./debts-table";
 import DebtsChart from "./debts-chart";
 import { createClient } from "@/lib/supabase/server";
+import Heading from "@/components/heading";
 
 export default async function DebtsPage() {
   const sb = await createClient();
@@ -8,8 +9,10 @@ export default async function DebtsPage() {
 
   return (
     <section className="px-4">
+      <Heading variant="h1">Debts</Heading>
       <DebtsTable debts={debts || []} />
       <div className="mt-14">
+        <Heading variant="h2">Debt Value Per Year</Heading>
         <DebtsChart debts={debts || []} />
       </div>
     </section>
