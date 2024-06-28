9e9c71ddb059c0e4dfb1dd25c224af064e745c4c
scottchen98 <scott.chen1117@gmail.com>
2024-06-28 16:15:53 -0700

    add toast notification for deleting goals and philanthropy in DeleteGoalsAndPhilanthropyButton

--- src/app/(authenticated)/dashboard/client/[id]/edit/(goals-and-philanthropy)/delete-goals-and-philanthropy-button.tsx
+++ src/app/(authenticated)/dashboard/client/[id]/edit/(goals-and-philanthropy)/delete-goals-and-philanthropy-button.tsx
@@ -5,6 +5,7 @@ import { useServerAction } from "zsa-react";
 import DeleteItemButton from "@/components/delete-item-button";
 import { deleteGoal } from "./actions";
 import { useParams } from "next/navigation";
+import { toast } from "sonner";
 
 export default function DeleteGoalsAndPhilanthropyButton({
   id,
@@ -20,7 +21,19 @@ export default function DeleteGoalsAndPhilanthropyButton({
       size="icon"
       isPending={isPending}
       onClick={async () => {
-        await execute({ client_id: clientId, goal_id: id });
+        toast.promise(
+          execute({
+            client_id: clientId,
+            goal_id: id,
+          }),
+          {
+            loading: "Deleting...",
+            success: "Goal deleted successfully.",
+            error: (error) => {
+              if (error instanceof Error) return error.message;
+            },
+          }
+        );
       }}
     />
   );
