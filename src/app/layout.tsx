99786423c516ae34394f9c586a251bfa5c7dfa8d
scottchen98 <scott.chen1117@gmail.com>
2024-06-28 15:34:39 -0700

    update Toaster component in RootLayout to use top-center position

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={cn(inter.className, "antialiased")}>
         <Providers>{children}</Providers>
-        <Toaster richColors />
+        <Toaster position="top-center" richColors />
       </body>
     </html>
   );
